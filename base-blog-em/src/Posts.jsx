c6776b4308825ed791e151119f8faa44353458ea
Mxolisi Khumalo <mxolisi.khumalo@activeops.com>
2026-05-18 09:13:54 +0200

    used useQuery function to get posts

--- base-blog-em/src/Posts.jsx
+++ base-blog-em/src/Posts.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { useQuery } from "@tanstack/react-query";
 
 import { fetchPosts, deletePost, updatePost } from "./api";
 import { PostDetail } from "./PostDetail";
@@ -8,8 +9,15 @@ export function Posts() {
   const [currentPage, setCurrentPage] = useState(0);
   const [selectedPost, setSelectedPost] = useState(null);
 
-  // replace with useQuery
-  const data = [];
+  // replace with useQuery, destructuring the data property that is returned from the query function we pass to useQuery
+  const {data} = useQuery({  // Takes an object of objetcs
+    queryKey: ["posts"],    // Defines the data inside the query cache, always an array. Only defines the data after query function has returned, till then data is undefined
+    queryFn: fetchPosts,    // The function that's going to run to fetch the data
+  });
+
+  if (!data) {
+    return <div />
+  }
 
   return (
     <>
