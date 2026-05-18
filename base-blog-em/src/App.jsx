15512af927978fe55229c5a4876224c48f87b727
Mxolisi Khumalo <mxolisi.khumalo@activeops.com>
2026-05-18 08:44:34 +0200

    created a query client and provided it as a prop.

--- base-blog-em/src/App.jsx
+++ base-blog-em/src/App.jsx
@@ -1,13 +1,19 @@
 import { Posts } from "./Posts";
 import "./App.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();    // Create a query client
 
 function App() {
   return (
-    // provide React Query client to App
-    <div className="App">
-      <h1>Blog &apos;em Ipsum</h1>
-      <Posts />
-    </div>
+    // provide the Query client to App as a prop
+    <QueryClientProvider client={queryClient}>
+      {/* Any thing inside this query client div is a descendant and can use the queryClient prop just passed */}
+      <div className="App">
+        <h1>Blog &apos;em Ipsum</h1>
+        <Posts />
+      </div>
+    </QueryClientProvider>
   );
 }
 
