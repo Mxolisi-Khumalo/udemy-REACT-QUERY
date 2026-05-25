c636614d34e33b1301f451289d4d031680e13a34
Mxolisi Khumalo <mxolisi.khumalo@activeops.com>
2026-05-25 09:07:55 +0200

    saving proress

--- base-blog-em/src/PostDetail.jsx
+++ base-blog-em/src/PostDetail.jsx
@@ -3,7 +3,7 @@ import { fetchComments } from "./api";
 import "./PostDetail.css";
 import { useQuery } from "@tanstack/react-query";
 
-export function PostDetail({ post }) {
+export function PostDetail({ post, deleteMutation, updateMutation }) {  // add the delete mutation as a prop
   // replace with useQuery
   const {data, isLoading, isError, error} = useQuery({
     queryKey: ["comments", post.id],
@@ -26,7 +26,26 @@ export function PostDetail({ post }) {
   return (
     <>
       <h3 style={{ color: "blue" }}>{post.title}</h3>
-      <button>Delete</button> <button>Update title</button>
+      <div>
+        <button onClick={() => deleteMutation.mutate(post.id)}>Delete</button> {/*Run the deleteMutation when delete button is clicked*/}
+        {deleteMutation.isPending && (
+          <p className="loading">Deleting the post...</p>
+        )}
+        {deleteMutation.isError && (
+          <p className="error">
+            Error deleting the post: {deleteMutation.error.toString()}
+          </p>
+        )}
+        {deleteMutation.isSuccess && (
+          <p className="success">Post was deleted(not)</p>
+        )}
+      </div>
+      <div>
+        <button onClick={() => updateMutation.mutate(post.id)}>Update title</button>
+        {updateMutation.isPending && (<p className="loading">Updating post title...</p>)}
+        {updateMutation.isError && (<p className="error">Error pudating title: {updateMutation.error.toString()}</p>)}
+        {updateMutation.isSuccess && (<p className="success">Title was updated</p>)}
+      </div>
       <p>{post.body}</p>
       <h4>Comments</h4>
       {data.map((comment) => (
--- base-blog-em/src/Posts.jsx
+++ base-blog-em/src/Posts.jsx
@@ -1,18 +1,38 @@
-import { useState } from "react";
-import { useQuery } from "@tanstack/react-query";
+import { useState, useEffect } from "react";
+import { useQuery, useQueryClient, useMutation } from "@tanstack/react-query";
 
 import { fetchPosts, deletePost, updatePost } from "./api";
 import { PostDetail } from "./PostDetail";
 const maxPostPage = 10;
 
 export function Posts() {
-  const [currentPage, setCurrentPage] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
   const [selectedPost, setSelectedPost] = useState(null);
+
+  const queryClient = useQueryClient();
+
+  const deleteMutation = useMutation({  // Create the deleteMutation hook using the useMutation 
+    mutationFn: (postId) => deletePost(postId), // pass the deletePost funciton as the mutation function to execute
+  });
+
+  const updateMutation = useMutation({
+    mutationFn: (postId) => updatePost(postId),
+  });
+
+  useEffect(() => { // The use effect hook detects when there is a change in a specific element and executes
+    if (currentPage < maxPostPage){
+      const nextPage = currentPage + 1; // we are prefetching data for the next page when we get to the new one
+      queryClient.prefetchQuery({
+        queryKey: ["posts", nextPage],  // the query key must have a similar structure so we can check if the data is in cache or not
+        queryFn: () => fetchPosts(nextPage),  // fecth posts data for the
+      });
+    }
+  }, [currentPage, queryClient]);   // There are the elements who's change triggers the useEffect
 
   // destructuring the data property that is returned from the query function we pass to useQuery
   const {data, isError, error, isLoading} = useQuery({  // Takes an object of objetcs
-    queryKey: ["posts"],    // Defines the data inside the query cache, always an array. Only defines the data after query function has returned, till then data is undefined
-    queryFn: fetchPosts,    // The function that's going to run to fetch the data
+    queryKey: ["posts", currentPage],    // Defines the data inside the query cache, always an array. Only defines the data after query function has returned, till then data is undefined
+    queryFn: () => fetchPosts(currentPage),    // The function that's going to run to fetch the data
     staleTime: 2000,        // specifying the stale time for the data ["posts"] that is fected, 2000ms => 2 seconds
   });
 
@@ -36,23 +56,28 @@ export function Posts() {
           <li
             key={post.id}
             className="post-title"
-            onClick={() => setSelectedPost(post)}
+            onClick={() => {
+              deleteMutation.reset();
+              updateMutation.reset();
+              setSelectedPost(post);
+            }}
           >
             {post.title}
           </li>
         ))}
       </ul>
       <div className="pages">
-        <button disabled onClick={() => {}}>
-          Previous page
+        <button disabled={currentPage<=1} onClick={() => {
+          setCurrentPage((previousValue) => previousValue - 1);}}>Previous page
         </button>
-        <span>Page {currentPage + 1}</span>
-        <button disabled onClick={() => {}}>
-          Next page
+        <span>Page {currentPage}</span>
+        <button disabled={currentPage >= maxPostPage} onClick={() => {
+          setCurrentPage(previousValue => previousValue + 1)}}> Next page
         </button>
       </div>
       <hr />
-      {selectedPost && <PostDetail post={selectedPost} />}
+      {/* Pass the deleteMutation hook to the PostDetail component as that is where it is gonna be executed */}
+      {selectedPost && <PostDetail post={selectedPost} deleteMutation={deleteMutation} updateMutation={updateMutation} />} 
     </>
   );
 }
